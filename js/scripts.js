acb88f7528282f7c1767932bcb9d6593f3cab122
koscielniakolaewa <31932678+koscielniakolaewa@users.noreply.github.com>
2017-10-04 12:00:25 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,6 @@ var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that firs
 var dinosaur = 'triceratops';
 var dinosaurUpperCased = dinosaur.toUpperCase();
 var textZamiana = text.replace('Velociraptor', dinosaurUpperCased);
-var cztextZamiana = textZamiana.slice(0,72);
+var dlugosc = textZamiana.length/2;
+var cztextZamiana = textZamiana.slice(0,dlugosc);
 console.log(cztextZamiana);
